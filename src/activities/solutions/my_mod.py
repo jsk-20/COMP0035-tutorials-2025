c5385f61156f785f3485bd816bd0398fa32b0b18
Jaihan Khurll <zceikhu@ucl.ac.uk>
2025-10-17 16:51:46 +0100

    Reading the csv

--- src/activities/solutions/my_mod.py
+++ src/activities/solutions/my_mod.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pandas as pd
 # import csv
 
 project_root = Path(__file__).parent.parent
@@ -10,3 +11,57 @@ print(csv_file.exists())
 #     reader = csv.reader(f, delimiter=',')
 #     first_row = next(reader)
 #     print(first_row)
+
+
+xlsx_file = project_root.joinpath('data', 'paralympics_all_raw.xlsx')
+# print(xlsx_file.exists())
+
+sheet1 = pd.read_excel(xlsx_file)
+sheet2 = pd.read_excel(xlsx_file, sheet_name=1)
+
+variable_name_for_df = pd.read_csv(csv_file)
+# print(variable_name_for_df)
+
+
+def describe_dataframe(df):
+    """Summary or description of the function
+        Parameters:
+        argument1 (int): Description of argument1
+        Returns:
+        int: Description of the returning value
+    """
+    print("\n--- DataFrame Shape ---")
+    print(df.shape)  # (rows, columns)
+
+    print("\n--- First 5 Rows ---")
+    print(df.head())
+
+    print("\n--- Last 5 Rows ---")
+    print(df.tail())
+
+    print("\n--- Column Labels ---")
+    print(df.columns.tolist())
+
+    print("\n--- Column Data Types ---")
+    print(df.dtypes)
+
+    print("\n--- DataFrame Info ---")
+    df.info()
+
+    print("\n--- Descriptive Statistics ---")
+    print(df.describe())
+
+
+# dataframe(sheet1)
+
+if __name__ == "__main__":
+    # Filepath of the csv data file (you may have used importlib.resources
+    # rather than pathlib.Path)
+    paralympics_csv = (
+        Path(__file__).parent.parent.joinpath("data", "paralympics_raw.csv")
+    )
+    # Read the data from the file into a Pandas dataframe
+    events_csv_df = pd.read_csv(paralympics_csv)
+    # Call the function named 'describe_dataframe' - you may have a different
+    # name for your function
+    describe_dataframe(events_csv_df)
