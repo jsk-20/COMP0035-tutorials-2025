d6988767d2fb3b8a3f0b916d60a9dd9ccd135470
Jaihan Khurll <zceikhu@ucl.ac.uk>
2025-10-19 21:12:56 +0100

    simple plot

--- src/activities/starter/example_plot.py
+++ src/activities/starter/example_plot.py
@@ -9,9 +9,12 @@ if __name__ == '__main__':
         'B': [4, 3, 2, 1]
     })
 
-    # Using pandas.plot directly creates the figure, axes and allows for some customisation
-    # matplotlib examples typically split this into separate commands defining fig and ax then adding customisation
-    ax = df.plot(title='Sample Plot', xlabel='X-axis Label', ylabel='Y-axis Label')
+    # Using pandas.plot directly creates the figure, axes and allows for some
+    # customisation
+    # matplotlib examples typically split this into separate commands defining
+    # fig and ax then adding customisation
+    ax = df.plot(title='Sample Plot', xlabel='X-axis Label',
+                 ylabel='Y-axis Label')
 
     # Show the plot
     plt.show()
